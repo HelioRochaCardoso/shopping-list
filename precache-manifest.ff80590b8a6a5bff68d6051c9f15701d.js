cbe1635aa2536ba3ac37b8a83504816c3550d83d
Helio Cardoso <cardoso_helio@hotmail.com>
2020-07-30 14:00:08 +0100

    Updates

---
+++ precache-manifest.ff80590b8a6a5bff68d6051c9f15701d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a62d4ab8d71a48ec7c8f269efde946",
+    "url": "/shopping-list/index.html"
+  },
+  {
+    "revision": "a041a4319a8d2252b113",
+    "url": "/shopping-list/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "861b63d96b0224aecf14",
+    "url": "/shopping-list/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "a041a4319a8d2252b113",
+    "url": "/shopping-list/static/js/2.bc84cd2a.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/shopping-list/static/js/2.bc84cd2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "861b63d96b0224aecf14",
+    "url": "/shopping-list/static/js/main.efab970b.chunk.js"
+  },
+  {
+    "revision": "eb411cefcdc5faffd8b2",
+    "url": "/shopping-list/static/js/runtime-main.40efd9c2.js"
+  }
+]);
